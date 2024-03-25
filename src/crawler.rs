60fe03da60fc3ec2a6e2c07d8c56df41c5972b7b
Rishikesh01 <53863619+Rishikesh01@users.noreply.github.com>
2024-03-25 22:14:01 +0530

    WIP

--- src/crawler.rs
+++ src/crawler.rs
@@ -1,14 +1,20 @@
-use std::sync::Arc;
+use std::{
+    borrow::Borrow,
+    collections::HashSet,
+    sync::{Arc, Mutex},
+};
 
 use crossbeam_channel::{bounded, Receiver, Sender};
 use reqwest::Error;
 use scraper::{Html, Selector};
 
 const VALID_PATTERN: &str = "/store/apps/details?id=";
+const PLAY_STORE_URL: &str = "https://play.google.com";
 
 pub struct PlayStoreCrawler {
     client: reqwest::Client,
     sender: Sender<String>,
+    filter: Mutex<HashSet<String>>,
     reciever: Receiver<String>,
 }
 
@@ -18,6 +24,7 @@ impl PlayStoreCrawler {
         Self {
             client,
             sender,
+            filter: Mutex::new(HashSet::new()),
             reciever: receiever,
         }
     }
@@ -25,32 +32,53 @@ impl PlayStoreCrawler {
     async fn do_request(&self, url: String) -> Result<String, Error> {
         match self.client.get(url).send().await?.text().await {
             Ok(body) => Ok(body),
-            Err(e) => Err(e),
-        }
-    }
+            Err(e) => {
+                println!("Error receiving URL: {:?}", e);
 
-    async fn scrape(&self) {
-        if let Ok(url) = self.reciever.recv() {
-            if let Ok(body) = self.do_request(url.to_string()).await {
-                let document = Html::parse_document(&body);
-                let selector = Selector::parse(".Si6A0c").unwrap();
-                for element in document.select(&selector) {
-                    if let Some(path) = element.value().attr("href") {
-                        if path.starts_with(VALID_PATTERN) {
-                            self.sender.send(path.to_string()).unwrap()
-                        }
-                    };
-                }
+                Err(e)
             }
         }
     }
 
+    async fn scrape(&self, num: i32) {
+        println!("{}", num);
+        loop {
+            match self.reciever.recv() {
+                Ok(url) => {
+                    if let Ok(body) = self.do_request(url.to_string()).await {
+                        let document = Html::parse_document(&body);
+                        match Selector::parse(".Si6A0c") {
+                            Ok(selector) => {
+                                println!("{} task num {} ", url, num);
+                                for element in document.select(&selector) {
+                                    if let Some(path) = element.value().attr("href") {
+                                        if path.starts_with(VALID_PATTERN) {
+                                            let full_url = format!("{}{}", PLAY_STORE_URL, path);
+
+                                            if let Err(err) = self.sender.send(full_url) {
+                                                eprintln!("Error sending path: {:?}", err);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            Err(_) => todo!(),
+                        }
+                    }
+                }
+                Err(err) => {
+                    eprintln!("Error receiving URL: {:?}", err);
+                    continue; // Continue looping even if there's an error receiving URL
+                }
+            }
+        }
+    }
     pub async fn start_scraping(self, url: &str) -> Result<(), Error> {
         self.sender.send(url.to_string()).unwrap();
         let crawler = Arc::new(self); // Wrap self in Arc
         let mut tasks = Vec::new();
-        for _ in 0..5 {
-            let task = tokio::spawn(PlayStoreCrawler::manage(Arc::clone(&crawler)));
+        for num in 0..5 {
+            let task = tokio::spawn(PlayStoreCrawler::manage(Arc::clone(&crawler), num));
             tasks.push(task);
         }
 
@@ -62,7 +90,7 @@ impl PlayStoreCrawler {
         Ok(())
     }
 
-    async fn manage(crawler: Arc<Self>) {
-        crawler.scrape().await;
+    async fn manage(crawler: Arc<Self>, num: i32) {
+        crawler.scrape(num).await;
     }
 }
