83bfffc6ee052e190db40c7966d0fa67e382cf36
Rishikesh01 <53863619+Rishikesh01@users.noreply.github.com>
2024-03-31 16:47:09 +0530

    WIP

--- src/crawler.rs
+++ src/crawler.rs
@@ -1,7 +1,6 @@
-use std::sync::Arc;
+use std::{error::Error, sync::Arc};
 
 use crossbeam_channel::{bounded, Receiver, Sender};
-use reqwest::Error;
 use scraper::{Html, Selector};
 
 const VALID_PATTERN: &str = "/store/apps/details?id=";
@@ -23,7 +22,7 @@ impl PlayStoreCrawler {
         }
     }
 
-    async fn do_request(&self, url: String) -> Result<String, Error> {
+    async fn do_request(&self, url: String) -> Result<String, reqwest::Error> {
         match self.client.get(url).send().await?.text().await {
             Ok(body) => Ok(body),
             Err(e) => {
@@ -34,66 +33,36 @@ impl PlayStoreCrawler {
         }
     }
 
-    async fn scrape(&self, sender: Sender<String>, receiver: Receiver<String>, num: i32) {
+    async fn scrape(&self, num: i32) -> Result<(), Box<dyn Error>> {
         println!("Task {}: Start scraping", num);
         loop {
-            match receiver.recv() {
-                Ok(url) => {
-                    println!("Task {}: Received URL: {}", num, url);
-                    match self.do_request(url.clone()).await {
-                        Ok(body) => {
-                            println!("Task {}: Successfully fetched URL: {}", num, url);
-                            let document = Html::parse_document(&body);
-                            match Selector::parse(".Si6A0c") {
-                                Ok(selector) => {
-                                    for element in document.select(&selector) {
-                                        if let Some(path) = element.value().attr("href") {
-                                            if path.starts_with(VALID_PATTERN) {
-                                                let full_url =
-                                                    format!("{}{}", PLAY_STORE_URL, path);
-                                                if let Err(err) = sender.send(full_url.clone()) {
-                                                    eprintln!(
-                                                        "Task {}: Error sending URL: {:?}",
-                                                        num, err
-                                                    );
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                                Err(err) => {
-                                    eprintln!("Task {}: Error parsing selector: {:?}", num, err);
-                                    // Handle the error, e.g., break the loop or continue
-                                }
-                            }
-                        }
-                        Err(err) => {
-                            eprintln!("Task {}: Error fetching URL: {:?}", num, err);
-                            // Handle the error, e.g., break the loop or continue
+            let url = self.receiver.recv().map_err(|e| {
+                Box::<dyn Error>::from(format!("error while receiving in url: {:?}", e))
+            })?;
+            let page = self.do_request(url.clone()).await?;
+            let document = Html::parse_document(&page);
+            let result = Selector::parse(".Si6A0c").map_err(|e| {
+                Box::<dyn Error>::from(format!("error while parsing page: {:?}", e))
+            })?;
+            for element in document.select(&result) {
+                if let Some(path) = element.value().attr("href") {
+                    if path.starts_with(VALID_PATTERN) {
+                        let full_url = format!("{}{}", PLAY_STORE_URL, path);
+                        if let Err(err) = self.sender.send(full_url.clone()) {
+                            eprintln!("Task {}: Error sending URL: {:?}", num, err);
                         }
                     }
-                }
-                Err(err) => {
-                    eprintln!("Task {}: Error receiving URL: {:?}", num, err);
-                    // Handle the error, e.g., break the loop or continue
                 }
             }
         }
     }
 
-    pub async fn start_scraping(self, url: &str) -> Result<(), Error> {
-        let (sender_clone, receiver_clone) = (self.sender.clone(), self.receiver.clone()); // Clone the sender and receiver
+    pub async fn start_scraping(self, url: &str) -> Result<(), reqwest::Error> {
         let mut tasks = Vec::new();
-        let crawler = Arc::new(self);
 
-        sender_clone.send(url.to_string()).unwrap();
+        self.sender.send(url.to_string()).unwrap();
         for num in 0..5 {
-            let sender_clone = sender_clone.clone();
-            let receiver_clone = receiver_clone.clone();
-            let crawler_clone = Arc::clone(&crawler);
-            let task = tokio::spawn(async move {
-                PlayStoreCrawler::manage(crawler_clone, sender_clone, receiver_clone, num).await;
-            });
+            let task = tokio::spawn(self.scrape(num));
 
             tasks.push(task);
         }
@@ -104,14 +73,5 @@ impl PlayStoreCrawler {
         }
 
         Ok(())
-    }
-
-    async fn manage(
-        crawler: Arc<Self>,
-        sender: Sender<String>,
-        receiver: Receiver<String>,
-        num: i32,
-    ) {
-        crawler.scrape(sender, receiver, num).await;
     }
 }
